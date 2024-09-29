0c8e8ec8901d21cd85398916744ae0879d1bda65
KingOfGhosts <priyanshu020210@gmail.com>
2024-09-29 15:33:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,11 +11,9 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name} 📁
 
-sɪᴢᴇ ⚙️: {file_size}
-
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz)</b>""" 
+⚙️ Sɪᴢᴇ : {file_size} ⚙️</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
